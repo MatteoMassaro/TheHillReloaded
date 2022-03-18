c212922d005d36d6a71619ea2f9296e52bd91aff
MatteoMassaro <massaromatteo21@gmail.com>
2022-03-18 18:42:15 +0100

    aggiustato multiplayer

--- app/src/main/java/com/example/thehillreloaded/menu/RisultatiActivity.java
+++ app/src/main/java/com/example/thehillreloaded/menu/RisultatiActivity.java
@@ -22,13 +22,13 @@ import com.google.firebase.database.ValueEventListener;
 public class RisultatiActivity extends Animazioni {
 
     //variabili
-    Button mostraRisultati, menu;
-    TextView testo;
+    Button mostraRisultati, aggiorna, menu;
+    TextView testo, testoTuoPunteggio, testoPunteggioAvversario;
     ProgressBar caricamento;
-    boolean player1IsPlaying, player2IsPLaying, mostraRisultatiPlayer1, mostraRisultatiPlayer2;
-    int scorePlayer1, scorePlayer2;
+    boolean player1IsPlaying, player2IsPLaying;
+    int scorePlayer1, scorePlayer2, playerExit;
     FirebaseDatabase database;
-    DatabaseReference ref, ref2;
+    DatabaseReference ref, ref2, ref3;
 
     @Override
     public void onBackPressed() {
@@ -41,8 +41,11 @@ public class RisultatiActivity extends Animazioni {
 
         //Trova le view tramite l'id e le assegna alle variabili
         mostraRisultati = findViewById(R.id.mostraRisultati);
+        aggiorna = findViewById(R.id.aggiorna);
         menu = findViewById(R.id.menu);
         testo = findViewById(R.id.testo);
+        testoTuoPunteggio = findViewById(R.id.testoTuoPunteggio);
+        testoPunteggioAvversario = findViewById(R.id.testoPunteggioAvversario);
         caricamento = findViewById(R.id.caricamento);
 
         //Imposta l'orientamento portrait come obbligatorio
@@ -50,55 +53,61 @@ public class RisultatiActivity extends Animazioni {
 
         //Animazione pulsanti
         clickButtonAnimation(mostraRisultati);
+        clickButtonAnimation(aggiorna);
         clickButtonAnimation(menu);
 
         database = FirebaseDatabase.getInstance();
 
-        if(MultigiocatoreActivity.unoVSunoClassico) {
-            ref = database.getReference("rooms_uno_contro_uno_classico").child(ConnessioneActivity.roomName);
-        }else{
-            ref = database.getReference("rooms_uno_contro_uno_powerUp").child(ConnessioneActivity.roomName);
-        }
-        ref.addValueEventListener(new ValueEventListener() {
+        aggiorna.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    player1IsPlaying = snapshot.child("player1_isPlaying").getValue(Boolean.class);
-                    ref.removeEventListener(this);
+            public void onClick(View view) {
+                if(MultigiocatoreActivity.unoVSunoClassico) {
+                    ref2 = database.getReference("rooms_uno_contro_uno_classico").child(ConnessioneActivity.roomName);
+                }else{
+                    ref2 = database.getReference("rooms_uno_contro_uno_powerUp").child(ConnessioneActivity.roomName);
                 }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        if(MultigiocatoreActivity.unoVSunoClassico) {
-            ref2 = database.getReference("rooms_uno_contro_uno_classico").child(ConnessioneActivity.roomName);
-        }else{
-            ref2 = database.getReference("rooms_uno_contro_uno_powerUp").child(ConnessioneActivity.roomName);
-        }
-        ref2.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    player2IsPLaying = snapshot.child("player2_isPlaying").getValue(Boolean.class);
-                    ref.removeEventListener(this);
-                    if (GameView.playerExit == 0) {
-                        if (!player1IsPlaying && !player2IsPLaying) {
-                            mostraRisultati.setEnabled(true);
+                ref2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()) {
+                            player1IsPlaying = snapshot.child("player1_isPlaying").getValue(Boolean.class);
                         }
-                    } else {
-                        caricamento.setVisibility(View.GONE);
-                        mostraRisultati.setEnabled(true);
                     }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+                if(MultigiocatoreActivity.unoVSunoClassico) {
+                    ref3 = database.getReference("rooms_uno_contro_uno_classico").child(ConnessioneActivity.roomName);
+                }else{
+                    ref3 = database.getReference("rooms_uno_contro_uno_powerUp").child(ConnessioneActivity.roomName);
                 }
-            }
+                ref3.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()) {
+                            player2IsPLaying = snapshot.child("player2_isPlaying").getValue(Boolean.class);
+                            playerExit = snapshot.child("playerExit").getValue(Integer.class);
+                            if (playerExit == 0) {
+                                if (!player1IsPlaying && !player2IsPLaying) {
+                                    caricamento.setVisibility(View.GONE);
+                                    mostraRisultati.setEnabled(true);
+                                }
+                            } else {
+                                caricamento.setVisibility(View.GONE);
+                                mostraRisultati.setEnabled(true);
+                            }
+                        }
+                    }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
+                    }
+                });
             }
         });
 
@@ -107,13 +116,18 @@ public class RisultatiActivity extends Animazioni {
         mostraRisultati.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(MultigiocatoreActivity.unoVSunoClassico) {
+                    ref = database.getReference("rooms_uno_contro_uno_classico").child(ConnessioneActivity.roomName);
+                }else{
+                    ref = database.getReference("rooms_uno_contro_uno_powerUp").child(ConnessioneActivity.roomName);
+                }
                 ref.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if(snapshot.exists()) {
                             scorePlayer1 = snapshot.child("score_player1").getValue(Integer.class);
-                            ref.removeEventListener(this);
                         }
+                        ref.removeEventListener(this);
                     }
 
                     @Override
@@ -127,39 +141,55 @@ public class RisultatiActivity extends Animazioni {
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if(snapshot.exists()) {
                             scorePlayer2 = snapshot.child("score_player2").getValue(Integer.class);
-                            ref.removeEventListener(this);
-                            if (GameView.playerExit == 0) {
+                            if (playerExit == 0) {
                                 if (ConnessioneActivity.player1) {
                                     if (scorePlayer1 > scorePlayer2) {
                                         testo.setText(R.string.vittoria);
+                                        testoTuoPunteggio.setText(getResources().getString(R.string.tuo_punteggio) + " " + String.valueOf(scorePlayer1));
+                                        testoPunteggioAvversario.setText(getResources().getString(R.string.punteggio_avversario) + String.valueOf(scorePlayer2));
                                     } else if (scorePlayer1 < scorePlayer2) {
                                         testo.setText(R.string.sconfitta);
+                                        testoTuoPunteggio.setText(getResources().getString(R.string.tuo_punteggio) + " " + String.valueOf(scorePlayer1));
+                                        testoPunteggioAvversario.setText(getResources().getString(R.string.punteggio_avversario) + String.valueOf(scorePlayer2));
                                     } else {
                                         testo.setText(R.string.pareggio);
+                                        testoTuoPunteggio.setText(getResources().getString(R.string.tuo_punteggio) + " " + String.valueOf(scorePlayer1));
+                                        testoPunteggioAvversario.setText(getResources().getString(R.string.punteggio_avversario) + " " + String.valueOf(scorePlayer2));
                                     }
                                 }
 
                                 if (ConnessioneActivity.player2) {
                                     if (scorePlayer2 > scorePlayer1) {
                                         testo.setText(R.string.vittoria);
+                                        testoTuoPunteggio.setText(getResources().getString(R.string.tuo_punteggio) + " " + String.valueOf(scorePlayer2));
+                                        testoPunteggioAvversario.setText(getResources().getString(R.string.punteggio_avversario) + " " + String.valueOf(scorePlayer1));
                                     } else if (scorePlayer2 < scorePlayer1) {
                                         testo.setText(R.string.sconfitta);
+                                        testoTuoPunteggio.setText(getResources().getString(R.string.tuo_punteggio) + " " + String.valueOf(scorePlayer2));
+                                        testoPunteggioAvversario.setText(getResources().getString(R.string.punteggio_avversario) + " " + String.valueOf(scorePlayer1));
                                     } else {
                                         testo.setText(R.string.pareggio);
+                                        testoTuoPunteggio.setText(getResources().getString(R.string.tuo_punteggio) + " " + String.valueOf(scorePlayer2));
+                                        testoPunteggioAvversario.setText(getResources().getString(R.string.punteggio_avversario) + " " + String.valueOf(scorePlayer1));
                                     }
                                 }
                             } else {
-                                if (ConnessioneActivity.player1 && GameView.playerExit == 1) {
+                                if (ConnessioneActivity.player1 && playerExit == 1) {
                                     testo.setText(R.string.sconfitta);
-                                } else if (ConnessioneActivity.player1 && GameView.playerExit == 2) {
+                                    testoTuoPunteggio.setText(R.string.tuo_abbandono);
+                                } else if (ConnessioneActivity.player1 && playerExit == 2) {
                                     testo.setText(R.string.vittoria);
-                                } else if (ConnessioneActivity.player2 && GameView.playerExit == 1) {
+                                    testoTuoPunteggio.setText(R.string.abbandono_avversario);
+                                } else if (ConnessioneActivity.player2 && playerExit == 1) {
                                     testo.setText(R.string.vittoria);
+                                    testoTuoPunteggio.setText(R.string.abbandono_avversario);
                                 } else {
                                     testo.setText(R.string.sconfitta);
+                                    testoTuoPunteggio.setText(R.string.tuo_abbandono);
                                 }
                             }
                         }
+                        ref.removeEventListener(this);
                         menu.setVisibility(View.VISIBLE);
                     }
 
